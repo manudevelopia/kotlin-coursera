62ad4a837fba6b5f4363009affa4af925e6b49ff
Manu Garcia <info@developia.info>
2019-01-06 00:11:59 +0100

    Create a function an call it once

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -3,5 +3,9 @@ package intro
 fun main(args: Array<String>) {
     val name = if (args.isNotEmpty()) args[0] else "Kotlin"
 
-    println("Hello, $name!")
+    println("Hello, $name! or better said ${getNameUpcase(name)}!!!")
+}
+
+fun getNameUpcase(name: String) : String {
+    return name.toUpperCase()
 }
