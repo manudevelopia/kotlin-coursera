8fd425deebf00a447be836ebe110f5488e97ae0e
Manu Garcia <info@developia.info>
2019-01-08 11:43:12 +0100

    Refactor function and provide def value

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -4,8 +4,10 @@ fun main(args: Array<String>) {
     val name = if (args.isNotEmpty()) args[0] else "Kotlin"
 
     println("Hello, $name! or better said ${getNameUpcase(name)}!!!")
+
+    println("Who is there, maybe ${getNameUpcase()}???")
 }
 
-fun getNameUpcase(name: String) : String {
+fun getNameUpcase(name: String = "John Doe") : String {
     return name.toUpperCase()
 }
