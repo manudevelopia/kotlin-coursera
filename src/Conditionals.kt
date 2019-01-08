925e48aaa9c7512a04d56cbe1e0dca6f6da81208
Manu Garcia <manu@developia.info>
2019-01-08 13:33:59 +0100

    Refactor Conditionals to clarify and test concepts

--- src/Conditionals.kt
+++ src/Conditionals.kt
@@ -1,46 +1,52 @@
 package intro
 
-class Conditionals {
 
-    // if
+fun main(args: Array<String>) {
+    println(areYouOldEnough())
+    println(getDescription(Color.BLUE))
+    println(whenSyntax(0))
+    println(whenSyntax(1))
+    println(whenSyntax("string"))
+}
 
-    fun areYouOldEnough(age: Int = 0): Boolean {
+// if
+fun areYouOldEnough(age: Int = 0): Boolean {
 
-        val oldEnough = if (age > 18) true else false
+    val oldEnough = if (age > 18) true else false
 
-        // No ternary operator
-        // (age > 18) ? true : false
+    // No ternary operator
+    // (age > 18) ? true : false
 
-        return oldEnough
-    }
+    return oldEnough
+}
 
-    // when as swich
+// when as swich
 
-    enum class Color {
-        BLUE, ORANGE, RED
-    }
+enum class Color {
+    BLUE, ORANGE, RED
+}
 
-    fun getDescription(color: Color): String =
+fun getDescription(color: Color): String =
         when (color) {
             Color.BLUE -> "cold"
             Color.ORANGE -> "mild"
-            Color.RED -> {"hot"}
+            Color.RED -> {
+                "hot"
+            }
         }
 
-    // 'is' is java 'instanceof'
-    fun whenSyntax(a: Any) = when (a){
-        0 -> "is zero"
-        is Int -> "is integer"
-        is String -> "is string of lengh ${a.length}"
-        else -> "other"
-    }
+// 'is' is java 'instanceof'
+fun whenSyntax(a: Any) = when (a) {
+    0 -> "is zero"
+    is Int -> "is integer"
+    is String -> "is string of lengh ${a.length}"
+    else -> "other"
+}
 
-    fun updateWeather(degrees: Int) {
-        val (description, colour) = when {
-            degrees < 5 -> "cold" to Color.BLUE
-            degrees < 23 -> "mild" to Color.ORANGE
-            else -> "hot" to Color.RED
-        }
+fun updateWeather(degrees: Int) {
+    val (description, colour) = when {
+        degrees < 5 -> "cold" to Color.BLUE
+        degrees < 23 -> "mild" to Color.ORANGE
+        else -> "hot" to Color.RED
     }
-
 }
